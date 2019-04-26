fbff77f2cae214c923a8f5733ac27a839810d759
Jaikangam Malangmei <jaikangam.malangmei@rippleslearning.com>
2019-04-26 14:10:00 +0530

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        
+        $role = \Auth::user()->role;
+        
+        if ($role == 'admin') {         
+           return view('admin');
+        }
+        elseif ($role == 'agent') {         
+           return view('agent');
+        }
+        else 
+        {   
+           return view('customer');
+        }
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/admin', function(){
+   return view('admin');
+})->middleware('auth','admin');
+
+Route::get('/agent', function(){
+   return view('agent');
+})->middleware('auth','agent');
+ 
+Route::get('/customer', function(){
+    return view('customer');
+})->middleware('auth','customer');
